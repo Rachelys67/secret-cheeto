fc84c21fd234ca1983fa9309637655d55e544317
Zaralysx <71341346+Zaralysx@users.noreply.github.com>
2021-07-30 14:29:35 -0400

    Update draw.js

--- commands/draw.js
+++ commands/draw.js
@@ -16,7 +16,7 @@ module.exports = {
     description: 'draws 3 cards',
     execute(message, args) {
         const discCommand = message.content;
-        //console.log("hello??");
+        console.log("hello??");
 
         let facistMsg = new Discord.MessageEmbed()
             .setImage('../img/facisttile.png')
